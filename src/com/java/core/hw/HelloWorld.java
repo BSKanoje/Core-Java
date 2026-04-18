ee1ba938492c4343d04739ff811335b8e12fe2e5
BSKanoje <bushrakanoje1101@gmail.com>
2026-04-18 10:37:16 +0530

    Project creation

---
+++ src/com/java/core/hw/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.java.core.hw;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World..");
+		String name = "Irise";
+		System.out.println(name);
+	}	
+}
