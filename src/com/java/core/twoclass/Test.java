fedc8496e24e99eeaed4ba26803f46147de71747
BSKanoje <bushrakanoje1101@gmail.com>
2026-04-27 09:43:54 +0530

    Two class

--- src/com/java/core/twoclass/Test.java
+++ src/com/java/core/twoclass/Test.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
-		
+
 		// Requirement -> fname = John, lname = wick, yob = 1999
 		// username = Johnwick99
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		UsernameGenerator ug = new UsernameGenerator();
 		System.out.println("Enter first name, last name and year of birth :");
 		String fname = sc.next();
 		String lname = sc.next();
 		int yob = sc.nextInt();
-		
-		String username = ug.getUserName(fname, lname, yob);
-		System.out.println("Username is "+username);
-		
 		sc.close();
+
+		String username = ug.getUserName(fname, lname, yob);
+		System.out.println("Username is " + username);
+
 	}
 }
