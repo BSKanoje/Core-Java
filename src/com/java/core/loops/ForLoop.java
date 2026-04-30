59473631bb91e1e79c61ac8cf1deffbed4489173
BSKanoje <bushrakanoje1101@gmail.com>
2026-04-30 09:38:59 +0530

    For loop

---
+++ src/com/java/core/loops/ForLoop.java
@@ -0,0 +1,22 @@
+package com.java.core.loops;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		System.out.println("START");
+		for(int i = 1; i<=10; i++) {
+			System.out.println("hii "+i);
+		}
+		System.out.println("END");
+		
+		for(int i = 5; i > 0; i--) {
+			System.out.println(i);
+		}
+		
+		for(int i =0;  i >= 0; i++) {
+			System.out.println("infinite");
+		}
+	}
+
+}
